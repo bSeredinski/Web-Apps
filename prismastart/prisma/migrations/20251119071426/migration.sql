c80bcc3c715121fbe85598a561fe020f149bd552
bSeredzinski <145098443+bSeredinski@users.noreply.github.com>
2026-01-05 13:19:58 +0100

    Add files via upload

---
+++ prismastart/prisma/migrations/20251119071426/migration.sql
@@ -0,0 +1,41 @@
+/*
+  Warnings:
+
+  - You are about to drop the `user` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE `user`;
+
+-- CreateTable
+CREATE TABLE `Kategoria` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `nazwa` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Wpis` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `tytul` VARCHAR(191) NOT NULL,
+    `tresc` VARCHAR(191) NOT NULL,
+    `kategoriaId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Komentarz` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `tresc` VARCHAR(191) NOT NULL,
+    `wpisId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `Wpis` ADD CONSTRAINT `Wpis_kategoriaId_fkey` FOREIGN KEY (`kategoriaId`) REFERENCES `Kategoria`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Komentarz` ADD CONSTRAINT `Komentarz_wpisId_fkey` FOREIGN KEY (`wpisId`) REFERENCES `Wpis`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
